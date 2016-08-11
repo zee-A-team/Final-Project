21979479d554c0e4a6a5f6e827ccfdb473e64aa8
theYounger <kuyehara1@gmail.com>
2016-08-10 15:40:05 -1000

    testing migrations

--- server.js
+++ server.js
@@ -14,25 +14,17 @@ db.on('error', console.error.bind(console, "connect error:"));
 db.once('open', () => {
   console.log("Mongo reporting for duty!");
 });
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/api', (req, res) => {
-  var req = new XMLHttpRequest();
-  req.open('GET', '');
-  req.send();
-});
 
 app.get( '/test', ( req, res ) => {
     Animal.find( ( err, animals ) => {
       if (err) res.send(err);
       res.json(animals);
     });
-  })
+  });
 
 app.post( '/test', ( req, res ) => {
     var animal = new Animal();
