66c58a841c872f77759801f561f3d8b13cd4a697
Vincent <vincentnewkirk@gmail.com>
2016-08-19 10:47:53 -1000

    graphql works public api is ready

--- server.js
+++ server.js
@@ -3,18 +3,29 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Animal = require('./models/Animal');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const graffiti = require('@risingstack/graffiti');
+const schema = require('./models/schema.js');
+
+
 
 /*----------  MONGOOSE ORM SETUP   ----------*/
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/et');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/et');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error"));
 db.once('open', _ => console.log("Mongo reporting for duty!"));
 
+mongoose.Promise = global.Promise;
+
+
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(graffiti.express({
+  schema
+}));
 
 app.get( '/test', ( req, res ) => {
     Animal.find( ( err, animals ) => {
