cd57ccad9980df40ce71f25ea3eafd4cf12678b4
Vincent <vincentnewkirk@gmail.com>
2016-08-23 12:30:48 -1000

    testing out bundlejs production build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,33 +1,30 @@
-'use strict';
-const path = require('path');
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: [
     './entry.js',
   ],
   output: {
-    path: __dirname + 'public',
-    filename: 'bundle.js'
+    path: `${__dirname}public`,
+    filename: 'bundle.js',
   },
   module: {
     loaders: [{
-        test: /.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /(\.scss$|\.css$)/,
-        loaders: [
-          'style',
-          'css',
-          'sass'
-        ]
-      }
+      test: /.js?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /(\.scss$|\.css$)/,
+      loaders: [
+        'style',
+        'css',
+        'sass',
+      ],
+    },
     ],
     postcss: [
-      require('autoprefixer')
-    ]
+      require('autoprefixer'),
+    ],
   },
 };
