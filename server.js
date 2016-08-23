066b42fd54046f39382855ac1be8cd41b6498d7a
Vincent <vincentnewkirk@gmail.com>
2016-08-22 14:16:33 -1000

    set up env variable for digital ocean deployment

--- server.js
+++ server.js
@@ -13,27 +13,35 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const config = require('./webpack.config');
 const compiler = webpack(config);
+require('dotenv').config();
 
 /*----------  MONGOOSE ORM SETUP   ----------*/
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/et');
+mongoose.connect(process.env.DB_CONNECT || process.env.DB_CONNECT);
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 const path = require('path');
 
-db.on('error', console.error.bind(console, "connection error"));
-db.once('open', _ => console.log("Mongo reporting for duty!"));
-
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(webpackDevMiddleware(compiler, {
+if(process.env === 'production'){
+  app.use(express.static('public'));
+} else {
+  app.use(express.static('public'));
+  app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
     stats: {
     colors: true,
-  }
-}));
+    }
+  }));
+}
+
+
+db.on('error', console.error.bind(console, "connection error"));
+db.once('open', _ => console.log("Mongo reporting for duty!"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get('/', (req, res) => {
   return res.sendFile('public/index.html');
