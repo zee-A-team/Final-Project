178924f918aa4daa6596b7d6713e7888877c221f
Vincent <vincentnewkirk@gmail.com>
2016-08-23 12:09:46 -1000

    fixed weird merge conflicts. still testing deployment

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Animal = require('./models/Animal');
-<<<<<<< HEAD
-=======
 // const PORT = process.env.PORT || 3001;
->>>>>>> production-deploy
 const graffiti = require('@risingstack/graffiti');
 const schema = require('./models/schema');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-<<<<<<< HEAD
-const config = require('./webpack.config');
+// const config = require('./webpack.config');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-const compiler = webpack(config);
-=======
+// const PORT = process.env.PORT || 3001;
+// const compiler = webpack(config);
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
 // const config = require('./webpack.config');
->>>>>>> production-deploy
 require('dotenv').config();
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const PORT = isDeveloping ? 3001 : process.env.PORT;
@@ -28,7 +22,7 @@ let config;
 const fs = require('fs');
 
 /*----------  MONGOOSE ORM SETUP   ----------*/
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_CONNECT || process.env.DB_CONNECT);
 mongoose.Promise = global.Promise;
 
@@ -82,24 +76,5 @@ app.get('/', (req, res) => res.sendFile('public/index.html'));
 app.use(graffiti.express({
   schema,
 }));
-
-<<<<<<< HEAD
-app.get('/test', (req, res) => {
-  Animal.find((err, animals) => {
-    if (err) res.send(err);
-    return res.json(animals);
-  });
-});
-
-app.post('/test', (req, res) => {
-  const animal = new Animal();
-  animal.commonName = req.body.commonName;
-  animal.save((err) => {
-    if (err) return res.send(err);
-    return res.send(animal);
-  });
-});
-=======
->>>>>>> production-deploy
 
 app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
